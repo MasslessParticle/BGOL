1cfbbf2cb345baa0bb271338ef566822d42df7a3
Travis Patterson <masslessparticle@gmail.com>
2011-02-23 12:01:33 -0700

    First Commit

---
+++ scripts/bgol_game_surface.js
@@ -0,0 +1,119 @@
+GameSurface = function(numRowCol, canvas){
+	this.m_canvas_ = canvas;
+	this.m_numRowCol = numRowCol; 
+	this.m_table_ = this.generateGrid();
+	this.populateGrid(this.m_table_);
+};
+
+GameSurface.prototype.draw = function(){
+	var x_step = this.m_canvas_.width / this.m_numRowCol;
+	var y_step = this.m_canvas_.height / this.m_numRowCol;
+	var x = 0;
+	var y = 0;
+	
+	context = this.m_canvas_.getContext("2d");
+	
+	context.clearRect(0,0,this.m_canvas_.width, this.m_canvas_.height);
+	
+	for (i=0; i < this.m_numRowCol; i++){
+		for (j=0; j < this.m_numRowCol; j++){
+			if (this.m_table_[i][j]){
+				context.fillRect(x,y,x_step,y_step);
+			} else {
+				context.strokeStyle = "#eee";
+				context.strokeRect(x,y,x_step,y_step);
+			}
+			
+			y += y_step;
+		}
+		x += x_step;
+		y = 0;
+	}
+};
+
+GameSurface.prototype.generateGrid = function(){
+	var grid = new Array(this.m_numRowCol);
+	for (i = 0; i < grid.length; i++){
+		grid[i] = new Array(this.m_numRowCol);
+	}
+	return grid;
+};
+
+GameSurface.prototype.populateGrid = function(grid){
+	for(i = 0; i < grid.length; i++){
+		for (j = 0; j < grid[i].length; j++){
+			var num = Math.floor(Math.random()*2);
+			grid[i][j] = num;
+		}
+	}
+	return grid;
+};
+
+GameSurface.prototype.update = function(){
+	newState = this.generateGrid();	
+	
+	for(i = 0; i < this.m_numRowCol; i++){
+		for (j = 0; j < this.m_numRowCol; j++){
+			newState[i][j] = this.checkCoord(i, j);
+		}
+	}
+	
+	this.m_table_ = newState;
+	this.draw();
+};
+
+GameSurface.prototype.checkCoord = function(x, y){
+	numNeighbours = 0;
+	numNeighbours = this.countNeighbours(x,y);
+	
+	if(this.m_table_[x][y] && (numNeighbours < 2 || numNeighbours > 3)){
+			return false;
+	}
+	
+	if(!this.m_table_[x][y] && numNeighbours == 3){
+		return true;
+	}
+	
+	return this.m_table_[x][y];
+};
+
+GameSurface.prototype.countNeighbours = function(x, y) {
+	num = 0;
+	
+	if ((x - 1) >= 0){
+		num += this.countY(x - 1, y);
+	}
+	
+	if((x + 1) < this.m_numRowCol){
+		num += this.countY(x + 1, y);
+	}
+	
+	num += this.countY(x, y);
+	
+	if(this.m_table_[x][y]){
+		num--;
+	}
+			
+	return num;
+};
+
+/**
+ * counts all true y values for a given column
+ */
+GameSurface.prototype.countY = function(x,y){
+	num = 0;
+	
+	if((y - 1) >= 0 && this.m_table_[x][y-1]){
+		num++;
+	}
+	
+	if((y + 1) < this.m_numRowCol && this.m_table_[x][y+1]){
+		num++;
+	}
+	
+	if(this.m_table_[x][y]){
+		num++;
+	}
+	
+	return num;
+};
---
+++ scripts/bgol_impl.js
@@ -0,0 +1,67 @@
+BGOL = function(div){
+	this.__init__(div);
+};
+
+BGOL.NUM_ROW_COL = 100;
+BGOL.DEFAULT_WIDTH = 600;
+BGOL.DEFAULT_HEIGHT = 600;
+
+BGOL.prototype.__init__ = function(div){
+	this.m_div = div;
+	this.createCanvas(div);
+	this.createGameSurface();
+};
+
+BGOL.prototype.createGameSurface =function(){
+	this.m_gameSurface = new GameSurface(BGOL.NUM_ROW_COL, this.m_canvas_);
+	this.m_gameSurface.draw();
+};
+
+BGOL.prototype.reset = function() {
+	this.stop();
+	this.createGameSurface();
+};
+
+BGOL.prototype.start = function(){
+	thisObject = this;
+	this.m_gameSurface.update();
+	this.timer = setTimeout(function(){thisObject.start();}, 100);
+};
+
+BGOL.prototype.stop = function(){
+	clearTimeout(this.timer);
+};
+
+BGOL.prototype.createCanvas = function(div) {
+	div.innerHTML = "";
+
+	// If the div isn't already sized then inherit from our attrs or
+	// give it a default size.
+	if (div.style.width == '') {
+		div.style.width = BGOL.DEFAULT_WIDTH + "px";
+	}
+
+	if (div.style.height == '') {
+		div.style.height = BGOL.DEFAULT_HEIGHT + "px";
+	}
+
+	this.m_width_ = parseInt(div.style.width, 10);
+	this.m_height_ = parseInt(div.style.height, 10);
+	// The div might have been specified as percent of the current window size,
+	// convert that to an appropriate number of pixels.
+	if (div.style.width.indexOf("%") == div.style.width.length - 1) {
+		this.m_width_ = div.offsetWidth;
+	}
+	if (div.style.height.indexOf("%") == div.style.height.length - 1) {
+		this.m_height_ = div.offsetHeight;
+	}
+
+	// Create the canvas for interactive parts of the chart.
+	this.m_canvas_ = document.createElement("canvas");
+	this.m_canvas_.style.position = "absolute";
+	this.m_canvas_.width = this.m_width_;
+	this.m_canvas_.height = this.m_height_;
+
+	// The interactive parts of the graph are drawn on top of the chart.
+	div.appendChild(this.m_canvas_);
+};
